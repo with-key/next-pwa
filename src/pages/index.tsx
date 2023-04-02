5bcd0c7f7e3c34ff0e9a169f37354d18ca275e2b
with-key <rem9238@kakao.com>
2023-04-03 02:22:39 +0900

    modify: 불필요한 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from "react";
 
 const Home = () => {
-  useEffect(() => {
-    Notification.requestPermission((result) => {
-      console.log(result);
-      if (result === "granted") {
-        alert("Permissions granted");
-      } else {
-        alert("Permissions denied");
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   Notification.requestPermission((result) => {
+  //     console.log(result);
+  //     if (result === "granted") {
+  //       alert("Permissions granted");
+  //     } else {
+  //       alert("Permissions denied");
+  //     }
+  //   });
+  // }, []);
 
   return <div>index</div>;
 };
