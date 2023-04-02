e25ebbee99dba1154d5869f0689785d9c6c43c55
with-key <rem9238@kakao.com>
2023-04-02 23:35:39 +0900

    mafifest, default setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache.js"); //
 
-module.exports = nextConfig
+const isProduction = process.env.NODE_ENV === "production";
+
+const config = {
+  reactStrictMode: true,
+};
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  dest: "public",
+  // disable: !isProduction,
+  runtimeCaching: [], // 빈배열을 넣으면 기본값 설정
+  // runtimeCaching: runtimeCaching,
+})(config);
+
+module.exports = nextConfig;
